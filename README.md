439b849912cfa15a507439f3e7a90294f23b350d
Lxtharia <maud-lin@gmx.de>
2025-07-11 14:24:10 +0200

    Initial Commit
    Configure files, yeehaw!

---
+++ README.md
@@ -0,0 +1,12 @@
+# CPPixsort
+
+Another modular Pixelsorter made by me, written in C++
+
+## Features
+
+- [ ] Any
+
+
+# Yee Haw
+
+Yeee
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+    std::cout << "Hello World!" << std::endl;
+    // Include test.png
+
+    // Sort it
+    // TODO
+
+    // Save it to sorted-test.png
+
+    return 0;
+}
