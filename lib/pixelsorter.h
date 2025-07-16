ffe9071ae29ec8f29f1fd7ba3a2926414d53f065
Lxtharia <maud-lin@gmx.de>
2025-07-16 20:50:09 +0200

    Use unique_ptr; Deep copy pixelsorter

--- lib/pixelsorter.h
+++ lib/pixelsorter.h
@@ -10,25 +10,27 @@ class Pixelsorter {
 private:
 public:
 	bool inverse;
-	SortingAlgorithm* algo;
-	PathCreator* pathing;
+	unique_ptr<SortingAlgorithm> algo;
+	unique_ptr<PathCreator> pathing;
 
 	Pixelsorter()
 		:inverse(false)
-		,algo(new BubbleSort(SortingAlgorithm::Criteria::BRIGHTNESS))
-		,pathing(new LinePath(LinePath::Direction::DOWN))
+		,algo(make_unique<BubbleSort>(SortingAlgorithm::Criteria::BRIGHTNESS))
+		,pathing(make_unique<LinePath>(LinePath::Direction::DOWN))
 	{ }
-	Pixelsorter(PathCreator* pathing, SortingAlgorithm* algo, bool inverse = false)
+	Pixelsorter(PathCreator& pathing, SortingAlgorithm& algo, bool inverse = false)
 		:inverse(inverse)
-		,pathing(pathing)
-		,algo(algo)
+		,pathing(make_unique<PathCreator>(pathing))
+		,algo(make_unique<SortingAlgorithm>(algo))
 	{}
 	~Pixelsorter() {}
 	Pixelsorter(Pixelsorter& sorter)
 		:inverse(sorter.inverse)
-		,algo(sorter.algo)
-		,pathing(sorter.pathing)
-	{};
+	{
+		// Make deep copy
+		this->pathing = make_unique<PathCreator>(*sorter.pathing);
+		this->algo = make_unique<SortingAlgorithm>(*sorter.algo);
+	};
 
 	void sort_pixels(int w, int h, Span);
 };
